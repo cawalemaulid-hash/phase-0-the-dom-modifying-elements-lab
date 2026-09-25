a8c2faca72090c6ec62daf6c5d027686f713fd6a
cawale maulid <cawalemaulid@gmail.com>
2026-09-25 12:04:57 +0300

    challenging

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello, DOM!';
+document.body.appendChild(h1);
+
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
+
+
+
+
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+document.body.appendChild(ul);
+
